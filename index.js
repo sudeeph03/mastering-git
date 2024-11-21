d5c42fe9e84c82e16afa822125f79ecf1d5d2cc1
Sudeep <sudeephukkerikar234@gmail.com>
2024-11-21 17:48:12 +0530

    Modify index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+// Function to greet a user when he/she enters their name
+
+const username = prompt("Enter your name : ");
+
+function greetUser(name) {
+    console.log(`Hello, ${name}`);
+}
+
+greetUser(username);
